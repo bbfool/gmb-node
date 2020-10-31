45eeff3f29e76bf71fe106f6b31b51742a9621fb
bbfool <47930587+bbfool@users.noreply.github.com>
2020-10-30 19:49:14 -0600

    Update app.js
    add intentions

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     var message = 'Hosted in GitHub!  It works!\n',
         version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
+        intentions = 'this is what we are going to do here',
+        response = [message, intentions, version].join('\n');
     res.end(response);
 });
 server.listen();
