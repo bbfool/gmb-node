d3725607e4c236505b0295b256d354eb6b53ad79
bbfool <47930587+bbfool@users.noreply.github.com>
2020-10-30 21:30:36 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var http = require('http');
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'Hosted in GitHub!  It works!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        intentions = 'this is what we are going to do here',
+    
+    var message = 'Hosted in GitHub!  It works!',
+        version = 'NodeJS ' + process.versions.node ,
+        intentions = 'this is what we are going to do here ' + process.env.PORT,
         response = [message, intentions, version].join('\n');
     res.end(response);
 });
